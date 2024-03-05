c91d16aef275137373ac408f06d106693f7ca4ca
Mark Smith <smith.h.mark@gmail.com>
2024-03-05 15:49:37 -0700

    slightly clearer fmting

--- eval_helper.sh
+++ eval_helper.sh
@@ -3,8 +3,10 @@
 date > results.out
 for file in example_data/*.txt
 do
+  echo "" >> results.out
   echo "$file" >> results.out
   python3 planner.py -v "$file" >> results.out
+  echo "" >> results.out
   date >> results.out
 done
 
